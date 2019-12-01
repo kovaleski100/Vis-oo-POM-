034ada528021e079ca674cb0450d07d5bc4c85fa
giovaneaf <giovaneaf1995@gmail.com>
2019-12-01 12:35:47 -0300

    milagres não acontecem

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def contourVoting(luminances, normals, N):
 	phij = np.random.rand(N)*2*math.pi
 	alphaAcc = np.zeros(N) # fix this variable
 	kid = 0.5 			   # surface coefficient
-	EPS = 1e-3
+	EPS = 1e-5
 	running = True
 	wj = np.array([0, math.sin(phij[0]), math.cos(phij[0])])
 	while(running):
@@ -42,7 +42,35 @@ def contourVoting(luminances, normals, N):
 					running = True
 	return phij
 
-realImg = cv2.imread('t1.jpg')
+
+def findZenithAngle(angle, img, h, w):
+	pixel = [0, 0]
+	if(angle > math.pi/4 and angle <= 3*math.pi/4):
+		pixel[0] = int(round((angle-math.pi/4)/(3*math.pi/4-math.pi/4)*(h-1)))
+		pixel[1] = w-1
+	elif(angle > 3*math.pi/4 and angle <= 5*math.pi/4):
+		pixel[0] = h-1
+		pixel[1] = int(round((1 - ((angle-3*math.pi/4)/(5*math.pi/4-3*math.pi/4)))*(w-1)))
+	elif(angle > 5*math.pi/4 and angle <= 7*math.pi/4):
+		pixel[0] = int(round((1 - ((angle-5*math.pi/4)/(7*math.pi/4-5*math.pi/4)))*(h-1)))
+		pixel[1] = 0
+	else:
+		newAngle = angle if angle < 0 else angle+2*math.pi
+		pixel[0] = 0
+		pixel[1] = int(round((newAngle-7*math.pi/4)/(9*math.pi/4-7*math.pi/4)*(w-1)))
+	grayValue = -1
+	print(pixel)
+	newAngle = angle + math.pi/2 if angle + math.pi/2 <= 2*math.pi else angle + math.pi/2 - 2*math.pi
+	du = math.sin(newAngle)
+	dv = math.cos(newAngle)
+	while(img[pixel[0]][pixel[1]] == 0):
+		pixel = [round(pixel[0] + du), round(pixel[1] + dv)]
+		if(pixel[0] < 0 or pixel[0] >= h or pixel[1] < 0 or pixel[1] >= w):
+			break
+	print(pixel)
+	
+
+realImg = cv2.imread('a.png')
 h, w = realImg.shape[:2]
 
 '''img = cv2.imread('a.png')
@@ -58,8 +86,7 @@ mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
 img = img*mask2[:,:,np.newaxis]'''
 
 gray = cv2.cvtColor(realImg, cv2.COLOR_BGR2GRAY)
-a, gray = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
-newImg = copy.deepcopy(gray)
+a, newImg = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
 newImg[:] = 0
 
 pt = [(116, 130)]
@@ -82,6 +109,8 @@ for (x, y) in pt:
 		for i in range(len(du)):
 			nu = u + du[i]
 			nv = v + dv[i]
+			if(nu < 0 or nu >= h or nv < 0 or nv >= w):
+				continue
 			if(seen[nu][nv] == 0 and gray[nu][nv] > 0):
 				queue.append((nu, nv))
 			elif(gray[nu][nv] == 0):
@@ -150,13 +179,20 @@ for u in range(h):
 
 lum = np.array(lum)
 normals = np.array(normals)
+omegaj = contourVoting(lum, normals, 3)
+andImg = copy.deepcopy(newImg[:])
+andImg[andImg > 0] = 1
+andImg = gray*andImg
 
-ans = contourVoting(lum, normals, 3)
+for azimuth in omegaj:
+	zenith = findZenithAngle(azimuth, andImg, h, w)
+ans = [180.0/math.pi*x for x in omegaj]
+
 print(ans)
 
-'''plt.imshow(newImg), plt.show()
-plt.imshow(edge), plt.show()
-cv2.imshow("Converted Image", newImg)
+plt.imshow(andImg), plt.show()
+'''
+cv2.imshow("Converted Image", andImg)
 
 # waiting for key event
 cv2.waitKey(0)
